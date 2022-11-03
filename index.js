257f40ef9d8916241641c0d50f06621f51e6a444
Revan Rionaldo <91299304+revanrion08@users.noreply.github.com>
2022-11-03 09:17:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Menyalakan mesin kopi");
+console.log("Menggiling biji kopi");
+console.log("Memanaskan air");
+console.log("Mencampurkan air dan kopi");
+console.log("Menuangkan kopi ke dalam gelas");
+console.log("Menuangkan susu ke dalam gelas");
+console.log("Kopi Anda sudah siap!");
+
+import _ from 'lodash';
+import _ from 'node_modules';
+
+// Tampilkan nama Anda ke layar pada kode di bawah:
